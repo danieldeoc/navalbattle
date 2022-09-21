1964b650ef4166448e8397d895fae93696090879
danieldeoc <danieldeoc@gmail.com>
2022-09-21 11:17:28 +0200

    ship placment fixes in horizontal

--- js.js
+++ js.js
@@ -7,7 +7,6 @@ let shipPlacement = 0;
 let placebeShips = 15;
 let placedShips = 0;
 let modePlacement = "horizontal";
-var num = 1;
 
 ////////////////////////
 // DRAW THE TABLES AND CELLS
@@ -29,42 +28,90 @@ function changeMode(){
 // INICIA O JOGO
 document.getElementById("start").addEventListener('click', () => { 
     document.getElementById("gamePanel").innerHTML = "<div id='fase'>Place your ships!</div>";
- } )
+} )
 
- ////////////////////////////////////////////////
- // controla a quantidade de placementes de navios
- var totalShips = 15
- var boats = 8;
- var ships = 4;
- var sub = 2;
- var carrier = 1;
- function totalPlacementShips(){
+////////////////////////////////////////////////
+// controla a quantidade de placementes de navios e espaço ocupado
+let num = 1;
+var totalShips = 15
+var boats = 8;
+var ships = 4;
+var sub = 2;
+var carrier = 1;
+function totalPlacementShips(){
     var totalShips = boats + ships + sub + carrier;
     return totalShips;
+}
+
+ /////////////////////////////
+ // controla o placement dos navios na quebra de linha
+ var lineBreakStatus = false;
+ function lineBreakCheck(id, numPlace){
+    const endLines = [7,15,23,31,39,47,55,63];
+    var ocupiedSpace = (id + numPlace) - 1;
+    for (let index = 0; index < 8; index++) {
+        lineBreak = endLines[index];
+        if(id <= lineBreak){    
+            if(ocupiedSpace > lineBreak){
+                mensagem("linha ultrapassada");
+                lineBreakStatus = true;
+            }
+        }
+    }
+    occupiedPlace(id, numPlace);
+}
+
+/////////////////////////////
+ // controla o placement de lugares ocupados
+ var occupiedPlaceStatus = false;
+ function occupiedPlace(id, numPlace){
+    // valida se o lugar esta vazio na horizontal
+    let provId = id;
+    var empty = true
+    for (let index = 0; index < numPlace; index++) {
+        if(playerShips.has(provId)){
+            var empty = false;
+        }
+        provId++
+    }
+    if(!empty){
+        occupiedPlaceStatus = true;
+        mensagem("lugar ocupado");
+    }
  }
 
 /////////////////////
 // controla o placment dos navios do player
 function place(id){
     if( gameStep == 0){
-        if(boats > 0){
-            boats--;
-            document.getElementById("boats").textContent = boats;
-        } else if(ships > 0){
-            ships--;
-            num = 2;
-            document.getElementById("ships").textContent = ships;
-        } else if(sub > 0){
-            sub--;
-            num = 3;
-            document.getElementById("submarines").textContent = sub;
-        } else if(carrier > 0){
-            carrier--;
-            num = 4;
-            document.getElementById("carriers").textContent = carrier;
+        lineBreakCheck(id, num);
+        
+        // verifica linha e lugar
+        if( !lineBreakStatus && !occupiedPlaceStatus){
+            if(placebeShips > 7){
+                boats--;
+                document.getElementById("boats").textContent = boats;
+            } else if(placebeShips > 3){
+                ships--;
+                
+                document.getElementById("ships").textContent = ships;
+            } else if(placebeShips > 1){
+                sub--;
+                
+                document.getElementById("submarines").textContent = sub;
+            } else {
+                carrier--;                
+                document.getElementById("carriers").textContent = carrier;
+            }
+            if(totalShips >= 1){
+                addAditionalPlace(id, num);
+                totalShips = totalPlacementShips();
+            }
         }
-        addAditionalPlace(id, num);
-        totalShips = totalPlacementShips();
+        lineBreakStatus = false;
+        occupiedPlaceStatus = false;
+    } else{
+        
     }
 }
 
@@ -72,41 +119,25 @@ function putAiShips(){
 //    randomId = Math.floor(Math.random() * 10);
 }
 
+
+
+
 //////////////////////////////////////////////////
 // cotrola a adicao e posicao dos lugares
-function addAditionalPlace(id, num){
-    // valida se o navio esta na mesma linha, caso horizontal
-    const endLines = [7,15,23,31,39,47,55,63];
-    var ocupiedSpace = (id + num) - 1;
-    for (let index = 0; index < 8; index++) {
-        lineBreak = endLines[index];
-        if(id <= lineBreak){
-            if(ocupiedSpace > lineBreak){
-                 mensagem("linha ultrapassada");
-                 return;
-            }
-        }
+function addAditionalPlace(id, numPlace){
+    // adiciona o navio ao tabuleiro
+    for (let index = 0; index < numPlace; index++) {
+        document.getElementById(playerTime+id).classList.add("shipped")
+        playerShips.add(id);
+        id++
     }
-
-    // valida se o lugar esta vazio na horizontal
-    let provId = id;
-    var empty = true
-    for (let index = 0; index < num; index++) {
-        if(playerShips.has(provId)){
-            var empty = false;
-        }
-        provId++
+    // update the num size var
+    if(placebeShips === 8 || placebeShips === 4 || placebeShips === 2 ){
+        num++
+    } else if(placebeShips === 1){
+        gameStep = 1
     }
-    if(!empty){
-        mensagem("lugar ocupado");
-    } else {
-        for (let index = 0; index < num; index++) {
-            document.getElementById(playerTime+id).classList.add("shipped")
-            playerShips.add(id);
-            id++
-        }
-        placebeShips--;
-    }
+    placebeShips--;
 }
 
 ///////////////////
@@ -131,21 +162,19 @@ function mensagem(msg){
 
 ////////////////////
 // lights ship place
-function lightning(index){
-    if(gameStep == 0 && totalShips > 0){
-        
-        const ele = document.getElementById("p"+index)
+function lightning(lIndex){
+    if(gameStep == 0 && placebeShips > 0){
+        const ele = document.getElementById("p"+lIndex)
         ele.classList.add("light");
-        console.log(num)
-        if(boats === 0){
-            var num = 2;
-        }
-        if(boats == 1){
-            for (let bIndex = 0; bIndex < num; bIndex++) {
-                document.getElementById("p"+index).classList.add("light");
-                index++;
+        
+        for (let bIndex = 0; bIndex < num; bIndex++) {
+            if(lIndex > 63){
+                lIndex = 63
             }
+            document.getElementById("p"+lIndex).classList.add("light");
+            lIndex++;
         }
+        
     }
 }
 function lightoff(){
