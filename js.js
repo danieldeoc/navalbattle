9e1eef395aa9fcc0f71cc96d69ac6241035d996f
danieldeoc <danieldeoc@gmail.com>
2022-10-08 16:27:22 +0200

    new placement check with arrays

--- js.js
+++ js.js
@@ -1,14 +1,41 @@
 const root = document.getElementById("root"); // root
 let gameStep = 0; // etapa do jogo
+var actionMode = null;
+var mode = "horizontal"; // controla o modo de inserção de navios horizontal/vertical
+
 const playerShips = new Set([]); // navios do Player
 const aiShip = new Set([]); // navios do AI
+
+const tableCells = [
+    [0,1,2,3,4,5,6,7],
+    [8,9,10,11,12,13,14,15],
+    [16,17,18,19,20,21,22,23],
+    [24,25,26,27,28,29,30,31],
+    [32,33,34,35,36,37,38,39],
+    [40,41,42,43,44,45,46,47],
+    [48,49,50,51,52,53,54,55],
+    [56,57,58,59,60,61,62,63]
+]
+const tableColumns = [
+    [0,8,16,24,32,40,48,56],
+    [1,9,17,25,33,41,49,57],
+    [2,10,18,26,34,42,50,58],
+    [3,11,19,27,35,43,51,59],
+    [4,12,20,28,36,44,52,60],
+    [5,13,21,29,37,45,53,61],
+    [6,14,22,30,38,46,54,62],
+    [7,15,23,31,39,47,55,63]
+]
+
+
+
 let playerTime = "p";   // controla a vez do jogador
 let shipPlacement = 0;  // posicao da disposicao
 let placebeShips = 15; // numero de navios colocaveis
 let placedShips = 0; // numero de navios colocados
-let modePlacement = "horizontal"; // controla o modo de inserção de navios horizontal/vertical
 var lineBreakStatus = false; // status da quebra de linha, impede execucoes indevidas
 var lineBreakStatusLight = false // status da quebra de linha do efeito de sobreposição
+
 const endLinesH = [56,57,58,59,60,61,62,63]; // array de linhas horizontais
 const endLines = [7,15,23,31,39,47,55,63]; // array de linhas verticais
 
@@ -24,8 +51,8 @@ drawGame();
 
 /////////////////////////////
 function changeMode(){
-    modePlacement == "horizontal" ? modePlacement = "vertical" : modePlacement = "horizontal";
-    document.getElementById("pos").textContent = modePlacement;
+    mode == "horizontal" ? mode = "vertical" : mode = "horizontal";
+    document.getElementById("pos").textContent = mode;
 }
 
 ////////////////////////
@@ -36,7 +63,7 @@ document.getElementById("start").addEventListener('click', () => {
 
 ////////////////////////////////////////////////
 // controla a quantidade de placementes de navios e espaço ocupado
-let num = 1;
+let shipSize = 1;
 var totalShips = 15
 var boats = 8;
 var ships = 4;
@@ -50,40 +77,41 @@ function totalPlacementShips(){
 
  /////////////////////////////
  // controla o placement dos navios na quebra de linha
- function lineBreakCheck(id, numPlace, origin){
-    if(modePlacement == "horizontal"){
+ function lineBreakCheck(id, numPlace, actionMode){
+    if(mode == "horizontal"){
         var ocupiedSpace = (id + numPlace) - 1;
         for (let index = 0; index < 8; index++) {
             lineBreak = endLines[index];
             if(id <= lineBreak){    
                 if(ocupiedSpace > lineBreak){
-                    console.log(origin)
-                    ocupiedResult(origin);
+                    
+                    ocupiedResult(actionMode);
                 }
             }
         }
     } else {
-        var ocupiedSpaceH = (id + numPlace + 8) - 1;
+        var ocupiedSpaceH = (id + numPlace) - 1;
         for (let index = 0; index < 8; index++) {
             lineBreakH = endLinesH[index];
             if(id <= lineBreakH){    
+                
                 if(ocupiedSpaceH > lineBreakH){
-                    console.log(origin)
-                    ocupiedResult(origin);
+                    
+                    ocupiedResult(actionMode);
                 }
             }
         }
     }
-    if(origin == "click"){
+    if(actionMode == "click"){
         occupiedPlace(id, numPlace);
     }
 }
 
-function ocupiedResult(origin){
+function ocupiedResult(actionMode){
     //mensagem("linha ultrapassada");
-    if(origin == "click"){
+    if(actionMode == "click"){
         lineBreakStatus = true;
-    } else if(origin == "mouse"){
+    } else if(actionMode == "mouse"){
         lineBreakStatusLight = true;
     }
 }
@@ -96,7 +124,7 @@ function ocupiedResult(origin){
     // valida se o lugar esta vazio na horizontal
     let provId = id;
     var empty = true
-    if(modePlacement == "horizontal"){
+    if(mode == "horizontal"){
         for (let index = 0; index < numPlace; index++) {
             if(playerShips.has(provId)){
                 var empty = false;
@@ -121,8 +149,12 @@ function ocupiedResult(origin){
 /////////////////////
 // controla o placment dos navios do player
 function place(id){
+    actionMode = "click";
+
     if( gameStep == 0){
-        lineBreakCheck(id, num, "click");
+        shipSpacesPreview(id, shipSize)
+
+        lineBreakCheck(id, shipSize, "click");
        
         // verifica linha e lugar
         if( !lineBreakStatus && !occupiedPlaceStatus){
@@ -141,7 +173,7 @@ function place(id){
                 document.getElementById("carriers").textContent = carrier;
             }
             if(totalShips >= 1){
-                addAditionalPlace(id, num);
+                addAditionalPlace(id, shipSize);
                 totalShips = totalPlacementShips();
             }
         }
@@ -157,12 +189,34 @@ function putAiShips(){
 //    randomId = Math.floor(Math.random() * 10);
 
 }
+var preview_ocupiedSpaces_mouse = new Array([]);
+var preview_ocupiedSpaces = new Array([]);
+
+////////////////////////////////////////////
+/// Creates an array containing the cells a new ship will ocupy to compare these values with other intereactions
+function shipSpacesPreview(cellId, spaces){ // 30
+    for (let index = 0; index < spaces; index++) {
+        // calcula as adições ao array por modo de placement
+        if(mode == "horizontal"){
+            cellId++
+        } else{
+            let cellId = cellId + 8;
+        }
+        // adiciona ao array por action mode
+        if( actionMode == "click"){
+            preview_ocupiedSpaces.push(cellId);
+        } else {
+            preview_ocupiedSpaces_mouse.push(cellId);
+        }
+    }
+    
+}
 
 //////////////////////////////////////////////////
 // cotrola a adicao e posicao dos lugares
 function addAditionalPlace(id, numPlace){
     // adiciona o navio ao tabuleiro
-    if(modePlacement == "horizontal"){
+    if(mode == "horizontal"){
         for (let index = 0; index < numPlace; index++) {
             document.getElementById(playerTime+id).classList.add("shipped")
             playerShips.add(id);
@@ -178,7 +232,7 @@ function addAditionalPlace(id, numPlace){
     }
     // update the num size var
     if(placebeShips === 8 || placebeShips === 4 || placebeShips === 2 ){
-        num++
+        shipSize++
     } else if(placebeShips === 1){
         gameStep = 1
     }
@@ -205,42 +259,53 @@ function mensagem(msg){
     }, 4000)
 }
 
+/// define o numer de celulas a clarear
+function cellToFill(initCell, size){ // 32, 2
+    cells = [];
+    for (let index = 0; index < size; index++) { // 32, 33
+        if(mode == "horizontal"){
+            cells.push(initCell)
+            initCell++
+        } else {    // 32, 40
+            cells.push(initCell)
+            initCell = initCell + 8;
+        }
+    }
+    return cells;
+}
+
+
+
 ////////////////////
 // lights ship place
-function lightning(lIndex){
-    if(gameStep == 0 && placebeShips > 0){
-        const ele = document.getElementById(playerTime+lIndex);
-        ele.classList.add("light");
-        lineBreakCheck(lIndex, num, "mouse")
-        if( !lineBreakStatusLight ){
-            if(modePlacement == "horizontal"){
-                for (let bIndex = 0; bIndex < num; bIndex++) {
-                    if(lIndex > 63){
-                        lIndex = 63
-                    }
-                    document.getElementById(playerTime+lIndex).classList.add("light");
-                    lIndex++;
-                }
-            } else {
-                var hCol = lIndex;
-                for (let bIndex = 0; bIndex < num; bIndex++) {
-                    if(hCol > 63){
-                        hCol = 63
-                    }
-                    document.getElementById(playerTime+hCol).classList.add("light");
-                    hCol = hCol + 8;
-                }
-            }   
-        }
-        lineBreakStatusLight = false;
+function lightning(cellId_lg){
+    actionMode = "mouse";
+    cellToLight = cellToFill(cellId_lg, shipSize);
+    light = false;
+
+    let checker = (arr, target) => target.every(v => arr.includes(v));
+
+    for (let index = 0; index < tableCells.length; index++) {
+        if(checker(tableCells[index], cellToLight)){
+            light = true;
+            break;
+        }    
     }
+    // ilumina as celulas
+    if(light == true){
+        for (let index = 0; index < cellToLight.length; index++) {
+            document.getElementById(playerTime+cellToLight[index]).classList.add("light");
+        }
+    }
+    light = false;
 }
+///////////////////////////
+/// Desliga as luzes
 function lightoff(){
     var lights = document.querySelectorAll(".light")
     for (let index = 0; index < lights.length; index++) {
         lights[index].classList.remove("light")
     }
-    
 }
     
 function bomb(id){
